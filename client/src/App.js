f755a66331258fc4bc5db39014b4d0930fc6bcf1
ziieng <cjengelhardt@gmail.com>
2021-02-09 21:10:01 -0800

    adjust to match

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from './Components/Jumbotron/header'
 import Footer from './Components/Footer/footer'
 import Login from "./Components/Login/login";
 import Signup from "./Components/SignUp/signup";
-import React from "react";
+
 
 
 function App() {
