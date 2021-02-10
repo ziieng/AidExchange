b76f9187ebe72a1f94806a042b0349549f34ccaf
CarolY <mon_alisa75@yahoo.com>
2021-02-10 09:35:19 -0800

    Firebase configuration.

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDQt0WAXuPYYxPKknlw8QI7U63ytK11578",
+  authDomain: "aidexchange-bf872.firebaseapp.com",
+  projectId: "aidexchange-bf872",
+  storageBucket: "aidexchange-bf872.appspot.com",
+  messagingSenderId: "485723049370",
+  appId: "1:485723049370:web:2009b6adabecfd96ccbe23",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
