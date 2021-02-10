cfe667e5504d9f4dd23b29be0adaf8eda19e1b24
metages worku <metages09@gmail.com>
2021-02-09 18:49:25 -0800

    login and signu up pages layout

--- client/src/Components/NavBar/navbar.js
+++ client/src/Components/NavBar/navbar.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React from 'react'
+
+export default function navbar() {
+    return (
+        <div>
+            <nav className=" navbar-dark bg-dark ">
+                <h1 className=" text-center text-light">
+                    AidExchange
+                </h1>
+            </nav>
+        </div>
+    )
+}
