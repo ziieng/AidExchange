57757204e59a0ffd97a14c3b5bd0c86c5151e0f8
ziieng <cjengelhardt@gmail.com>
2021-02-11 11:48:33 -0800

    Added logout button component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login/login";
+import LogoutButton from "./Components/LogoutButton";
 import Signup from "./Components/SignUp/signup";
 import fire from "./firebaseConfig";
 
@@ -34,7 +35,7 @@ function App() {
           }
           <Switch>
             <Route path="/">
-              <p>Success!</p>
+              <LogoutButton />
             </Route>
           </Switch>
         </>
