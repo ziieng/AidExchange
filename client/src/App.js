273305b3ba3e67adeb28117e6a2d2c639a9ac71e
CarolY <mon_alisa75@yahoo.com>
2021-02-10 22:26:43 -0800

    Added FirebaseAuth from react-with-firebase-auth and bring in firebaseConfig

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Header from './Components/Jumbotron/header'
-import Footer from './Components/Footer/footer'
+import Header from "./Components/Jumbotron/header";
+import Footer from "./Components/Footer/footer";
 import Login from "./Components/Login/login";
 import Signup from "./Components/SignUp/signup";
-
-
+import withFirebaseAuth from "react-with-firebase-auth";
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
 
 function App() {
-    return (
-      <Router>
+  return (
+    <Router>
       <div>
-          < Header />
+        <Header />
         <Switch>
-          <Route exact path="/Signup"><Signup /></Route>
-          <Route exact path="/Login"><Login /></Route>
+          <Route exact path="/Signup">
+            <Signup />
+          </Route>
+          <Route exact path="/Login">
+            <Login />
+          </Route>
         </Switch>
-        < Footer />
+        <Footer />
       </div>
-
     </Router>
-    );
-  }
+  );
+}
 
-export default App;
+export default withFirebaseAuth({
+  firebaseAppAuth,
+})(App);
