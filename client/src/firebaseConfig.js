a198652aead3fbb7ca10f780b7768acfcc6157d5
ziieng <cjengelhardt@gmail.com>
2021-02-11 17:38:50 -0800

    Troubleshooting for console warnings

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,5 +1,5 @@
-import firebase from "firebase";
-require('firebase/auth')
+import firebase from "firebase/app";
+import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
