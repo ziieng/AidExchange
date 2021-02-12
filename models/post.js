b3ba14d696243d58c9b651e4e4579d4606fe45ca
ziieng <cjengelhardt@gmail.com>
2021-02-11 17:39:13 -0800

    Correct schema for GeoJSON objects

--- models/post.js
+++ models/post.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const pointSchema = new mongoose.Schema({
+  // reference: https://mongoosejs.com/docs/geojson.html
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true
+  },
+  coordinates: {
+    type: [Number],
+    required: true
+  }
+})
+
 const replySchema = new Schema({
   userId: { type: String, required: true }, //pull in ID from user posting it
   message: String, // let them say hi I guess
   replyType: { type: String, required: true }, //give or take
   contents: Array, //if they're only requesting part
-  location: {
-    type: {
-      type: String,
-      enum: ['Point'], // 'location.type' must be 'Point'
-    }
-  },
+  location: { type: pointSchema, required: true },
   createDate: { type: Date, default: Date.now },
 })
 
@@ -23,12 +31,7 @@ const postSchema = new Schema({
   acctType: { type: String, required: true }, //individual, org, charity
   postType: { type: String, required: true }, //supply or request
   // description: String, // do we want to add descriptions?
-  location: { // reference: https://mongoosejs.com/docs/geojson.html
-    type: {
-      type: String,
-      enum: ['Point'], // 'location.type' must be 'Point'
-    }
-  },
+  location: { type: pointSchema, required: true },
   createDate: { type: Date, default: Date.now },
   replies: [replySchema]
 });
