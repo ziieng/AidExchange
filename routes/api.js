e84ce488ef20538dfd46362eed9c5ea1d312b280
CarolY <mon_alisa75@yahoo.com>
2021-02-14 16:27:46 -0800

    Establish route for Post.

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,30 @@
 const router = require("express").Router();
-const userController = require("../controllers/userController");
+const { userController, postController } = require("../controllers");
 
 // Matches with "/api/user"
-router.route("/user")
-  .post(userController.create);
+router.route("/user").post(userController.create);
 
 // Matches with "/api/user/:id"
-// router
-  // .route("/user/:id")
-  // .get(userController.findById)
-  // .put(userController.update)
-  // .delete(userController.remove);
+router
+  .route("/user/:id")
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.remove);
+
+// Mathces with "/api/post"
+
+router
+  .route("/post")
+  .get(postController.findAllPost)
+  .post(postController.reservePost);
+
+// Mathces with "/api/post/:id"
+router
+  .route("/post/:id")
+  .get(postController.findPostById)
+  .put(postController.updatePost)
+  .delete(postController.removePost);
+
+// router.route().post("/email", userController.sendEmail);
 
 module.exports = router;
