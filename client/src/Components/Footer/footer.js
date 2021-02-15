583cfa61a4eb4e6e8d96dee82712e3e9bc916fdb
Alischrec <alischreck@gmail.com>
2021-02-15 13:32:37 -0800

    fixed profiledetails and navbar

--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer(props) {
     return (
-        <footer>
+        <footer className='text-center mt-4'>
             Copyright &copy; | 2021
         </footer>
     )
--- client/src/Components/NavBar/navbar.js
+++ client/src/Components/NavBar/navbar.js
@@ -5,7 +5,7 @@ export default function TopNav() {
   return (
     <Navbar className="navbar navbar-expand-lg navbar-dark">
       <Navbar.Brand href="">AidExchange</Navbar.Brand>
-        <Nav className="mr-auto">
+        <Nav className="justify-content-end" style={{ width: "100%" }}>
           <Nav.Link href="">Dashboard</Nav.Link>
           <Nav.Link href="">Profile</Nav.Link>
           <Nav.Link href="">Search</Nav.Link>
