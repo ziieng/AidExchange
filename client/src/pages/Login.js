1ebaa241f4b53818a903bdf3f0f39439306fb8c0
ziieng <cjengelhardt@gmail.com>
2021-02-17 15:43:57 -0800

    fix merging error

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useHistory } from "react-router-dom";
 import fire from '../firebase.js';
-import { Alert, Button, Col, Container, Form } from "react-bootstrap"
+import { Alert, Button, Card, Container, Form } from "react-bootstrap"
 
 
 export default function login() {
