2570da49d930542f58ef2830225064a78203d103
CarolY <mon_alisa75@yahoo.com>
2021-02-16 21:04:27 -0800

    Test stage for creating pdf.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const postController = require("../controllers/postController");
 const userController = require("../controllers/userController");
-const multer = require("multer");
+// const multer = require("multer");
 
 // const { Storage } = require('@google-cloud/storage');
 // const storage = new Storage({
@@ -9,12 +9,12 @@ const multer = require("multer");
 //     keyFilename: process.env.GCLOUD_APPLICATION_CREDENTIALS,
 // });
 
-const uploader = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024, // keep images size < 5 MB
-  },
-});
+// const uploader = multer({
+//   storage: multer.memoryStorage(),
+//   limits: {
+//     fileSize: 5 * 1024 * 1024, // keep images size < 5 MB
+//   },
+// });
 
 // Matches with "/api/user"
 router.route("/user").post(userController.create);
@@ -43,17 +43,32 @@ router
   .put(postController.updatePost)
   .delete(postController.removePost);
 
-router.post("/api/upload", uploader.single("image"), async (req, res, next) => {
-  try {
-    if (!req.file) {
-      res.status(400).send("No file uploaded.");
-      return;
-    }
-    // This is where we'll upload our file to Cloud Storage
-  } catch (error) {
-    res.status(400).send(`Error, could not upload file: ${error}`);
-    return;
-  }
-});
+// router.post("/api/upload", uploader.single("image"), async (req, res, next) => {
+//   try {
+//     if (!req.file) {
+//       res.status(400).send("No file uploaded.");
+//       return;
+//     }
+//     // This is where we'll upload our file to Cloud Storage
+//   } catch (error) {
+//     res.status(400).send(`Error, could not upload file: ${error}`);
+//     return;
+//   }
+// });
+
+//GENERATE PDF ROUTE
+// app.post('/create-pdf', (req, res) => {
+//   pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+//       if(err) {
+//           res.send(Promise.reject());
+//       }
+
+//       res.send(Promise.resolve());
+//   });
+// });
+
+// app.get('/fetch-pdf', (req, res) => {
+//   res.sendFile(`${__dirname}/result.pdf`)
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
+
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
