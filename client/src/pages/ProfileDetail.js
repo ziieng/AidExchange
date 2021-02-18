4e9d348f669508385404ae28a2afff0bc93b6028
ziieng <cjengelhardt@gmail.com>
2021-02-18 12:25:49 -0800

    population working for profile detail

--- client/src/pages/ProfileDetail.js
+++ client/src/pages/ProfileDetail.js
@@ -1,29 +1,80 @@
-import React from 'react';
-import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Card, Button } from 'react-bootstrap';
 import TopNav from '../Components/NavBar/navbar';
+import { useParams } from "react-router-dom"
+import API from "../utils/API"
+import Listing from "../Components/Cards/listing"
 
 export default function ProfileDetail(props) {
+    let { id } = useParams()
+    const [userPosts, setUserPosts] = useState([])
+    const [user, setUser] = useState({
+        "email": "",
+        "displayName": "",
+        "acctType": "",
+        "location": {},
+        "links": {},
+        "userId": ""
+    })
+
+    useEffect(() => {
+        loadUser()
+    }, [])
+
+    function loadUser() {
+        API.getUser(id)
+            .then(res => {
+                console.log(res)
+                setUser(res.data)
+            })
+        API.getUserListing({ params: { uid: id } })
+            .then(res => {
+                setUserPosts(res.data)
+            })
+    }
     return (
         <>
             < TopNav />
+            <Container>
             <Card className='profileDetails' >
                 <Card.Body>
-                    <Card.Img className='userImage' variant="top" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" />
-
-                    <Card.Title>Rad Organization</Card.Title>
-                            <Card.Link href="#">Website</Card.Link>
-                            <Card.Link href="#">Social Media</Card.Link>
-                            <Card.Link href="#">Venmo</Card.Link>
-                    <Card.Text>
-                        Description <br></br>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem></ListGroupItem>
-                            <ListGroupItem>Location</ListGroupItem>
-                        </ListGroup>
-                    </Card.Text>
+                        <Card.Img className='userImage' variant="top" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt={"user profile image for " + user.displayName} />
+                        <Card.Title>{user.displayName}</Card.Title>
+                        {user.links ? (
+                            <>
+                                {Object.entries(user.links).map(([key, value], i) => {
+                                    return (
+                                        <Card.Link key={i} href={value}>{key}</Card.Link>
+                                    );
+                                })}
+                            </>
+                        ) : (
+                                <Card.Subtitle>(No links provided)</Card.Subtitle>
+                            )}
+                        {user.description ? (
+                            <Card.Text>
+                                {user.description} <br></br>
+                            </Card.Text>
+                        ) : (
+                            <Card.Text>
+                                    (No description provided) <br></br>
+                        </Card.Text>
+                            )}
                     <Button className='editProfile' variant="dark">Edit Profile</Button>{' '}
                 </Card.Body>
             </Card>
+                {userPosts.length ? (
+                    <>
+                        {userPosts.map(post => {
+                            return (
+                                <Listing key={post._id} value={post} />
+                            );
+                        })}
+                    </>
+                ) : (
+                        <h3>No Listings to Display</h3>
+                    )}
+            </Container>
         </>
     )
 }
