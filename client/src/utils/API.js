089d23a08b833701df16b97a729adc68c83285b4
ziieng <cjengelhardt@gmail.com>
2021-02-18 12:23:05 -0800

    add route connection to get one user's details

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -13,19 +13,12 @@ export default {
   getListing: function (id) {
     return (axios.get("/api/post/" + id));
   },
-  // // Gets the book with the given id
-  // getBook: function (id) {
-  //   return axios.get("/api/books/" + id);
-  // },
-  // // Deletes the book with the given id
-  // deleteBook: function (id) {
-  //   return axios.delete("/api/books/" + id);
-  // },
-  // Saves a book to the database
+  getUser: function (id) {
+    return axios.get("/api/user/" + id);
+  },
   addUser: function (userData) {
     return axios.post("/api/user", userData);
   },
-
   // Saves a NewListing to the database
   addNewListing: function (newListData) {
     return axios.post("/api/NewListing", newListData);
