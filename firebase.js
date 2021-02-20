b4710b0cd9bb4381c6b4485d8b34802832960bd6
ziieng <cjengelhardt@gmail.com>
2021-02-20 15:29:23 -0800

    clean up unused code

--- firebase.js
+++ firebase.js
@@ -3,7 +3,6 @@ import "firebase/auth";
 import "firebase/storage";
 
 // Set the configuration for your app
-// TODO: Replace with your app's config object
 var firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
@@ -11,16 +10,3 @@ var firebaseConfig = {
   storageBucket: process.env.storageBucket,
 };
 firebase.initializeApp(firebaseConfig);
-
-// Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebase.storage();
-
-// Create a storage reference from our storage service
-var storageRef = storage.ref();
-
-// Create a child reference
-var imagesRef = storageRef.child("images");
-// imagesRef now points to 'images'
-
-// Child references can also take paths delimited by '/'
-var spaceRef = storageRef.child("images/space.jpg");
