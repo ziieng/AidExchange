e14c4db62740285fbe68ec53c9ec5cb46516f54c
CarolY <mon_alisa75@yahoo.com>
2021-02-20 23:04:11 -0800

    Added more props to pass listing values.

--- client/src/utils/document/index.js
+++ client/src/utils/document/index.js
@@ -7,7 +7,14 @@ import {
   Document,
   Page,
   StyleSheet,
+  Font,
 } from "@react-pdf/renderer";
+
+// Register font
+Font.register({
+  family: "Roboto",
+  src: "https://fonts.googleapis.com/css?family=Open+Sans|Roboto",
+});
 
 // Create styles
 const styles = StyleSheet.create({
@@ -21,15 +28,26 @@ const styles = StyleSheet.create({
     padding: 10,
     flexGrow: 1,
   },
+
   title: {
-    // big and bold?
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "semibold",
+    // fontFamily: "Roboto",
+  },
+  subtitle: {
+    fontSize: 18,
+    margin: 12,
   },
 });
 
 const MyDocument = (props) => (
   <Document>
     <Page size="A4">
-      <Text style={styles.heading}>{props.title}</Text>
+      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.heading}>
+        Your {props.postType} of : {props.title}{" "}
+      </Text>
       <Text style={styles.page}>
         {props.contents.map((line, i) => {
           return (
@@ -50,8 +68,11 @@ const Print = (props) => {
         // key={listing.userId}
         document={
           <MyDocument
+            postType={props.listing.postType}
+            displayName={props.listing.postBy.displayName}
             title={props.listing.title}
             contents={props.listing.contents}
+            status={props.listing.status}
           />
         }
         fileName="example.pdf"
