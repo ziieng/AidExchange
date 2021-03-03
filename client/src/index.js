9f8ec1c5b2cdb273ab5b43cd79e0d566db210636
ziieng <cjengelhardt@gmail.com>
2021-03-03 09:11:50 -0800

    troubleshooting heroku page error

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 // import { PDFViewer } from "@react-pdf/renderer"
 import "./index.css";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
